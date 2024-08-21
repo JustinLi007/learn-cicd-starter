fc26853b85ae3277613a887a52f7d016348d14b5
justinli007 <justinli2432@gmail.com>
2024-08-20 22:44:21 -0400

    Add security issues

--- json.go
+++ json.go
@@ -27,5 +27,10 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	b, err := w.Write(dat)
+	if err != nil {
+		log.Printf("Failed to write data to reply: %v", err)
+		return
+	}
+	log.Printf("Bytes ?: %v", b)
 }
